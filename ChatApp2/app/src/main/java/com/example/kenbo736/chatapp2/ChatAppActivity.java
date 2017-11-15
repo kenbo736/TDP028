44dffde793a76283fbad253e95015aeca3e03868
kenbo736 <kenbo736@student.liu.se>
2017-11-15 16:18:37 +0100

    working on invites

--- ChatApp2/app/src/main/java/com/example/kenbo736/chatapp2/ChatAppActivity.java
+++ ChatApp2/app/src/main/java/com/example/kenbo736/chatapp2/ChatAppActivity.java
@@ -1,6 +1,7 @@
 package com.example.kenbo736.chatapp2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +31,7 @@ public class ChatAppActivity extends AppCompatActivity {
     private Button sendButton;
     private Button signoutButton;
     private Button changeUsernameButton;
+    private Button sendSpamButton;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference dataRef;
@@ -71,6 +74,20 @@ public class ChatAppActivity extends AppCompatActivity {
             }
         });
 
+        sendSpamButton = (Button) findViewById(R.id.sendSpamButton);
+        sendSpamButton.setText(R.string.send_spam);
+        sendSpamButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
+                        .setMessage(getString(R.string.invitation_message))
+                        .setDeepLink(Uri.parse("http://google.com"))
+                        .setCustomImage(Uri.parse("https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTcAL_uXqNGDhlNIt0wsKBphnYi_G_lNgvWGjm1ZpEzt6T1OHWICnMjSbCLMVj.H5bGPhbofTc8000L_kG4cpIZtcLKYcKE9zco58YzufwifoFFPTqhAOxbFF1Mql6iZU1_d_FjrtR8s22JwBM3LkNpvdVFM_gV8XN1OLcbKQZGBvQ-&w=200&h=300&format=jpg"))
+                        .setCallToActionText(getString(R.string.invitation_cta))
+                        .build();
+                startActivityForResult(intent, 100);
+            }
+        });
+
         sendButton = (Button) findViewById(R.id.sendButton);
         sendButton.setText(R.string.send);
         sendButton.setOnClickListener(new View.OnClickListener() {
@@ -85,11 +102,6 @@ public class ChatAppActivity extends AppCompatActivity {
                 dataRef.child(key).setValue(map);
                 messageBox.setText("");
 
-                /*
-                chatWindow.append("\n" + mAuth.getCurrentUser().getEmail() + ": " + message);
-                dataRef.setValue(chatWindow.getText().toString());
-                */
-
             }
         });
         signoutButton = (Button) findViewById(R.id.signoutButton);
@@ -100,5 +112,23 @@ public class ChatAppActivity extends AppCompatActivity {
                 startActivity(new Intent(ChatAppActivity.this, MainActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Toast.makeText(ChatAppActivity.this, id, Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                // Sending failed or it was canceled, show failure message to the user
+                // ...
+            }
+        }
     }
 }
